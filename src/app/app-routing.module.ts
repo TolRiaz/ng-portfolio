4708aac30ab8827919ae3709797d02cdcb4ebf8f
tolriaz <zjaekrxj7@gmail.com>
2022-11-10 03:58:24 +0000

    add pages routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module')
+      .then( m => m.PagesModule )
+  },
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
