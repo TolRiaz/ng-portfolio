0f2ca039e17fc904dccba4db0febb50b9efa7245
tolriaz <zjaekrxj7@gmail.com>
2022-11-10 08:00:07 +0000

    still making sidebar

--- src/app/pages/layout/sidebar/sidebar.component.ts
+++ src/app/pages/layout/sidebar/sidebar.component.ts
@@ -10,21 +10,6 @@ export class SidebarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const body = document.querySelector("body");
-
-    // add class 'hover-open' to sidebar navitem while hover in sidebar-icon-only menu
-    document.querySelectorAll(".sidebar .nav-item").forEach(function(el) {
-      el.addEventListener("mouseover", function() {
-        if (body?.classList.contains("sidebar-icon-only")) {
-          el.classList.add("hover-open");
-        }
-      });
-      el.addEventListener("mouseout", function() {
-        if (body?.classList.contains("sidebar-icon-only")) {
-          el.classList.remove("hover-open");
-        }
-      });
-    });
   }
 
   public parentId = "";
